af26524ff0e20eb5f3541af4c21378b3cbd80330
Enrique Hernandez <enriquehernndez@Enriques-MacBook-Pro.local>
2017-11-13 15:33:01 -0800

    attempt at ticTacToe

---
+++ index.js
@@ -0,0 +1,9 @@
+//spent most of the time finding out how to build a cli
+
+console.log('Welcome, These are your options: \n\n' +
+    ' A | B | C \n' +
+    ' --------- \n' +
+    ' D | E | F \n' +
+    ' --------- \n' +
+    ' G | H | I \n');
+
